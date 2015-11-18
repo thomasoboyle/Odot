61ce4658f9060fe2214f637f3279042a92c8c96f
Thomas O'Boyle <thomashoboyle@gmail.com>
2015-11-17 20:45:43 -0800

    Added a name (title) validation to the create page and wrote a test for it

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -2,30 +2,32 @@ require 'spec_helper'
 
 describe "Creating todo lists" do
 	 it "redirects to the todo list index page on success" do
-	 	visit "/todo_lists"
-	 	#click_link "New Todo list"
-	 	expect(page).to have_content("New Todo list")
+	 visit "/todo_lists"
+	 click_link "New Todo list"
+	 expect(page).to have_content("New Todo List")
 
-	 	fill_in "Title", with: "My todo list"
-	 	fill_in "Description", with: "This is what I'm doing today."
-	# 	click_button "Create Todo list"
+	 fill_in "Title", with: "My todo list"
+	 fill_in "Description", with: "This is what I'm doing today."
+	 click_button "Create Todo list"
 
-	# 	expect(page). to have_content("My todo list")
-	end
+	 expect(page).to have_content("My todo list")
+end
 
-	it "displays an error when the todo list has no title" do
-		# expect(TodoList.count).to eq(0)
-		# it "redirects to the todo list index page on success" do
-		# visit "/todo_lists"
-		# click_link "New Todo list"
-		# expect(page).to have_content("New todo_list")
+	it "displays an error when the todo list as no title" do
+	 expect(TodoList.count).to eq(0)
 
-		# fill_in "Title", with: "My todo list"
-		# fill_in "Description", with: "This is what I'm doing today."
-		# click_button "Create Todo list"
-		# raise "Boom!"
+	 visit "/todo_lists"
+	 click_link "New Todo list"
+	 expect(page).to have_content("New Todo List")
 
-		# expect(page). to have_content("My BIG list")
-	#end
+	 fill_in "Title", with: ""
+	 fill_in "Description", with: "This is what I'm doing today."
+	 click_button "Create Todo list"
+
+	 expect(page).to have_content("error")
+	 expect(TodoList.count).to eq(0)
+
+	 visit "/todo_lists"
+	 expect(page).to_not have_content("This is what I'm doing today.")
 	end
 end
