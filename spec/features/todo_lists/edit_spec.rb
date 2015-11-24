b148d32a79659407c30d37016949b06e41ca7829
Thomas O'Boyle <thomashoboyle@gmail.com>
2015-11-23 22:41:30 -0800

    Adds tests to the edit page

--- spec/features/todo_lists/edit_spec.rb
+++ spec/features/todo_lists/edit_spec.rb
@@ -1,17 +1,27 @@
 require 'spec_helper'
 
 describe "Editting todo lists" do
-	it "updates a todo list successfully with the correct information" do
-		todo_lists = TodoList.create(title: "Groceries", description: "Grocery list.")
+	def update_todo_list(options={})
+		options[:title] ||= "My todo list"
+		options[:description] ||= "This is my todo list."
+
+		todo_list = options[:todo_list]
 
 		visit "/todo_lists"
-		within "#todo_list_#{todo_lists.id}" do
+		within "#todo_list_#{todo_list.id}" do
 			click_link "Edit"
 		end
 
 		fill_in "Title", with: "New title"
 		fill_in "Description", with: "New Description"
 		click_button "Update Todo list"
+	end
+
+	it "updates a todo list successfully with correct information" do
+		todo_lists = TodoList.create(title: "Groceries", description: "Grocery list.")
+		update_todo_list todo_list: todo_lists,
+						 title: "New title",
+						 description: "New description"
 
 		todo_lists.reload
 
