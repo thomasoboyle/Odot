7a06e2e56069926aea2103074ea8771c0b4ec865
Thomas O'Boyle <thomashoboyle@gmail.com>
2015-12-06 15:43:45 -0800

    Add todo item model and associated specs

--- Gemfile
+++ Gemfile
@@ -17,5 +17,6 @@ end
 
 group :test do
 	gem 'capybara', '~>2.1.0'
+	gem 'shoulda-matchers', '~>3.0.1'
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -64,3 +64,10 @@ end
 RSpec.configure do |c|
         c.expose_current_running_example_as :example
       end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
