f3c1b068af0d5944005161c4b4ec6b682487105e
Thomas O'Boyle <thomashoboyle@gmail.com>
2015-12-05 22:13:20 -0800

    Made a change to application.rb that allowed me to launch the server

--- config/application.rb
+++ config/application.rb
@@ -19,13 +19,6 @@ module Odot
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
-        RSpec.configure do |c|
-        c.expose_current_running_example_as :example
-      end
-
-  end
+    I18n.enforce_available_locales = false
+    end
 end
